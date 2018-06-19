fbe127b985477baa281a991102f9416e85a72ecd
dnirodhad <40340783+dnirodhad@users.noreply.github.com>
2018-06-19 13:51:32 +0530

    Add files via upload

---
+++ mtr.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    printf("University of Moratuwa\n");
+    printf("Katubadda,\n");
+    printf("Moratuwa,\n");
+    printf("Sri Lanka.,\n");
+    printf("---------------,\n");
+    printf("www.mtr.ac.lk.,\n");
+    return 0;
+}
